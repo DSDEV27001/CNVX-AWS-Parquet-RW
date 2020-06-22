e064995dbaf14de237067cc1e7158ac2d3a3368a
DSDEV27001 <52406362+DSDEV27001@users.noreply.github.com>
2020-06-22 04:15:11 +0100

    Created bucket security policy

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,20 @@
-FROM amazonlinux:2
+FROM python:3.8.3-slim-buster
 MAINTAINER PyDevUK
 
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
-RUN yum -y upgrade
-RUN yum -y install gcc groff less unzip wget python37 shadow-utils
-RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-RUN unzip awscliv2.zip
-RUN rm awscliv2.zip
+RUN apt-get -y update \
+    && apt-get -y install --no-install-recommends gcc groff less unzip wget curl \
+    && rm -rf /var/lib/apt/lists/*
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" \
+    && unzip awscliv2.zip && rm awscliv2.zip
 RUN ./aws/install
 #RUN wget https://www.python.org/ftp/python/3.8.3/Python-3.8.3.tgz
 #RUN tar -xf Python-3.8.3.tgz
 #RUN rm Python-3.8.3.tgz
 RUN pip3 install -r /requirements.txt
-RUN yum clean all
-RUN rm -rf /var/cache/yum
+RUN apt-get clean
 RUN python3 --version
 
 RUN mkdir /app
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
 import boto3
+from botocore.exceptions import ClientError
+import logging
 import pyarrow
-import smart_open
+
+BUCKET = "md123"
+REGION = "eu-west-2"
+s3_client = boto3.client("s3", region_name=REGION)
 
 
 def df_to_parquet(df_in: pd.DataFrame, filepath: str, idx: bool = False, **kwargs):
@@ -10,24 +15,83 @@ def df_to_parquet(df_in: pd.DataFrame, filepath: str, idx: bool = False, **kwarg
     df_in.to_parquet(path=filepath, index=idx, engine="pyarrow", **kwargs)
 
 
-url = 's3://md1234/test.txt'
+# url = "s3://md1234/data.parquet"
 
+
+def create_bucket(
+    bucket_name: str, region: str = None, acl_type: str = "private"
+) -> bool:
+    """Create an S3 bucket in a specified region
+
+    If a region is not specified, the bucket is created in the S3 default
+    region (us-east-1).
+
+    :param bucket_name: Bucket to create
+    :param region: String region to create bucket in, e.g., 'us-west-2'
+    :return: True if bucket created, else False
+    """
+
+    # Create bucket
+    try:
+        if region is None:
+            s3_client = boto3.client("s3")
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client("s3", region_name=region)
+            location = {"LocationConstraint": region}
+            s3_client.create_bucket(
+                Bucket=bucket_name, CreateBucketConfiguration=location, ACL=acl_type
+            )
+    except ClientError as e:
+        # S3.Client.exceptions.BucketAlreadyExists
+        # S3.Client.exceptions.BucketAlreadyOwnedByYou
+        logging.error(e)
+        return False
+    return True
+
+
+def encrypt_bucket(bucket_name: str):
+    response = s3_client.put_bucket_encryption(
+        Bucket=bucket_name,
+        ServerSideEncryptionConfiguration={
+            "Rules": [
+                {"ApplyServerSideEncryptionByDefault": {"SSEAlgorithm": "AES256"}},
+            ]
+        },
+    )
+    print(response)
+
+
+def block_bucket_public_access(bucket_name: str):
+    s3_client.put_public_access_block(Bucket=bucket_name, PublicAccessBlockConfiguration={"BlockPublicAcls": True,"IgnorePublicAcls": True,"BlockPublicPolicy": True,"RestrictPublicBuckets": True})
+
+
+create_bucket(BUCKET, REGION)
+encrypt_bucket(BUCKET)
+block_bucket_public_access(BUCKET)
+
+# response = s3_client.create_access_point(
+#     AccountId='string',
+#     Name='string',
+#     Bucket='string',
+#     VpcConfiguration={
+#         'VpcId': 'string'
+#     },
+#     PublicAccessBlockConfiguration={
+#         'BlockPublicAcls': True|False,
+#         'IgnorePublicAcls': True|False,
+#         'BlockPublicPolicy': True|False,
+#         'RestrictPublicBuckets': True|False
+#     }
+# )
+
+
+url = f"s3://{BUCKET}/data.parquet"
 # randomly generate an integer DataFrame
 df_rand = pd.DataFrame(
     np.random.randint(0, 999, size=(100000, 10)), columns=list("ABCDEFGHIJ")
 )
 
-df_rand.to_parquet(url)
+df_rand.to_parquet(url, engine="pyarrow")
 
-
-# def file_to_s3():
-#     # client = boto3.client("s3")
-#     session = boto3.session.Session()
-#     url = 's3://md1234/test.txt'
-#     with smart_open.open(url, 'wb', transport_params={'session': session}) as fout:
-#         bytes_written = fout.write(b'hello world!')
-#     print(bytes_written)
-
-
-
-
+print("3")
